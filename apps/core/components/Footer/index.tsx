0d820fd1ee433ec7ddd14be00725eaa7fffefbb0
Aleksander Mirowsky <alex.xande10@gmail.com>
2021-08-19 20:55:27 -0300

    [feature][refactor] implemented new footer

---
+++ apps/core/components/Footer/index.tsx
@@ -0,0 +1,290 @@
+import React from 'react';
+import { Box, IconButton } from '@material-ui/core';
+import {
+  Facebook,
+  Instagram,
+  WhatsApp,
+  YouTube,
+  LinkedIn,
+  Twitter,
+} from '@material-ui/icons';
+
+interface FooterProps {
+  FooterContactInfoProps?: FooterContactInfoProps;
+  FooterIconsProps?: FooterIconsProps;
+  FooterLinksProps?: FooterAdditionalLinksProps;
+}
+
+const Footer = ({
+  FooterContactInfoProps,
+  FooterIconsProps,
+  FooterLinksProps,
+}: FooterProps) => {
+  return (
+    <Box
+      className="Atlas-Footer-rootContainer"
+      sx={{
+        backgroundColor: (theme) => theme.palette.primary.main,
+        width: '100%',
+        borderTop: (theme) => `0.5px solid ${theme.palette.grey[400]}`,
+      }}
+    >
+      <Box
+        className="Atlas-Footer-upperFlexContainer"
+        sx={{
+          width: { xs: '100%', lg: '75% ' },
+          margin: 'auto',
+          display: 'flex',
+          flexDirection: { xs: 'column', lg: 'row' },
+          justifyContent: { xs: 'center', lg: 'space-evenly' },
+          alignItems: { xs: 'center', lg: 'flex-start' },
+          py: { xs: '2.5em' },
+          color: (theme) => theme.palette.primary.contrastText,
+          gap: { xs: '2.5em', lg: '0em' },
+        }}
+      >
+        <FooterContactInfo {...FooterContactInfoProps} />
+
+        {FooterLinksProps && <FooterAdditionalLinks {...FooterLinksProps} />}
+      </Box>
+
+      <Box
+        className="Atlas-Footer-bottomFlexContainer"
+        sx={{
+          width: '100%',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'column',
+          py: { xs: '1.5em' },
+          borderTop: (theme) => `0.5px solid ${theme.palette.grey[400]}`,
+        }}
+      >
+        <Box
+          className="Atlas-Footer-bottomFlexContainer-socialIconContainer"
+          sx={{
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            gap: '25px',
+            py: { xs: '1.5em' },
+          }}
+        >
+          <FooterIcons {...FooterIconsProps} />
+        </Box>
+
+        <Box
+          component={'p'}
+          sx={{
+            color: '#fff',
+            textTransform: 'capitalize',
+            fontSize: { xs: '10px', lg: '16px' },
+            textAlign: 'center',
+          }}
+        >{`© ${new Date().getFullYear()} Todos direitos reservados - Instituto Educacional Gnosis `}</Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default Footer;
+
+interface FooterAdditionalLinksProps {
+  label: string;
+  links: string[];
+}
+
+const FooterAdditionalLinks = ({
+  label,
+  links,
+}: Partial<FooterAdditionalLinksProps>) => {
+  return (
+    <Box
+      className="Atlas-Footer-upperflexContainer-additionalLinks"
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        fontSize: { xs: '12px', lg: '14px' },
+        gap: '10px',
+      }}
+    >
+      <Box
+        className="Atlas-Footer-upperflexContainer-additionalLinks-label"
+        sx={{ fontWeight: '800' }}
+      >
+        {label}
+      </Box>
+      <Box
+        className="Atlas-Footer-upperflexContainer-additionalLinks-list"
+        sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+      >
+        {links &&
+          links.map((value, index) => {
+            return <Box key={index}> {value} </Box>;
+          })}
+      </Box>
+    </Box>
+  );
+};
+
+interface FooterContactInfoProps {
+  address: string;
+  registration: string;
+  phones: string[];
+  emails: string[];
+}
+
+const FooterContactInfo = ({
+  address,
+  emails,
+  phones,
+  registration,
+}: Partial<FooterContactInfoProps>) => {
+  return (
+    <Box
+      className="Atlas-Footer-upperFlexContainer-contactInfo"
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontSize: { xs: '12px', lg: '14px' },
+        gap: '10px',
+      }}
+    >
+      <Box sx={{ fontWeight: '800' }}>Contato</Box>
+      <Box className="Atlas-Footer-upperFlexContainer-contactInfoAddress">
+        {address && <Box>{address} </Box>}
+      </Box>
+      <Box className="Atlas-Footer-upperFlexContainer-contactInfoRegistration">
+        {registration && <Box>{registration}</Box>}
+      </Box>
+      <Box className="Atlas-Footer-upperFlexContainer-contactInfoPhones">
+        {phones && (
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              flexDirection: 'column',
+            }}
+          >
+            {phones.map((value, index) => {
+              return <Box key={index}>{value}</Box>;
+            })}
+          </Box>
+        )}
+      </Box>
+
+      <Box className="Atlas-Footer-upperFlexContainer-contactInfoEmails">
+        {emails && (
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            {emails.map((value, index) => {
+              return <Box key={index}>{value}</Box>;
+            })}
+          </Box>
+        )}
+      </Box>
+    </Box>
+  );
+};
+
+interface FooterIconsProps {
+  facebook: string;
+  instagram: string;
+  whatsApp: string;
+  twitter: string;
+  youtube: string;
+  linkedin: string;
+}
+
+const FooterIcons = ({
+  facebook,
+  instagram,
+  whatsApp,
+  twitter,
+  youtube,
+  linkedin,
+}: Partial<FooterIconsProps>) => {
+  return (
+    <>
+      <IconButton
+        className="Atlas-Footer-facebookIcon"
+        sx={{
+          color: (theme) => theme.palette.primary.contrastText,
+          fontSize: { xs: '16px', lg: '30px' },
+        }}
+        href={facebook}
+        target="_blank"
+      >
+        <Facebook sx={{ fontSize: 'inherit' }} />
+      </IconButton>
+
+      <IconButton
+        className="Atlas-Footer-instagramIcon"
+        sx={{
+          color: (theme) => theme.palette.primary.contrastText,
+          fontSize: { xs: '16px', lg: '30px' },
+        }}
+        href={instagram}
+        target="_blank"
+      >
+        <Instagram sx={{ fontSize: 'inherit' }} />
+      </IconButton>
+
+      <IconButton
+        className="Atlas-Footer-whatsAppIcon"
+        sx={{
+          color: (theme) => theme.palette.primary.contrastText,
+          fontSize: { xs: '16px', lg: '30px' },
+        }}
+        href={whatsApp}
+        target="_blank"
+      >
+        <WhatsApp sx={{ fontSize: 'inherit' }} />
+      </IconButton>
+
+      <IconButton
+        className="Atlas-Footer-twitterIcon"
+        sx={{
+          color: (theme) => theme.palette.primary.contrastText,
+          fontSize: { xs: '16px', lg: '30px' },
+        }}
+        href={twitter}
+        target="_blank"
+      >
+        <Twitter sx={{ fontSize: 'inherit' }} />
+      </IconButton>
+
+      <IconButton
+        className="Atlas-Footer-youtubeIcon"
+        sx={{
+          color: (theme) => theme.palette.primary.contrastText,
+          fontSize: { xs: '16px', lg: '30px' },
+        }}
+        href={youtube}
+        target="_blank"
+      >
+        <YouTube sx={{ fontSize: 'inherit' }} />
+      </IconButton>
+
+      <IconButton
+        className="Atlas-Footer-linkedinIcon"
+        sx={{
+          color: (theme) => theme.palette.primary.contrastText,
+          fontSize: { xs: '16px', lg: '30px' },
+        }}
+        href={linkedin}
+        target="_blank"
+      >
+        <LinkedIn sx={{ fontSize: 'inherit' }} />
+      </IconButton>
+    </>
+  );
+};
--- apps/core/layout/LandingPage.layout.tsx
+++ apps/core/layout/LandingPage.layout.tsx
@@ -3,6 +3,7 @@ import { Box } from '@material-ui/core';
 import _ from 'lodash';
 import React from 'react';
 import Contact from '../components/Contact';
+import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
@@ -26,9 +27,26 @@ const AppLayout = ({ children }: AppLayoutProps) => {
           blobColor="secondary"
         />
       </div>
-      <StandardFooter
-        emails={['teste@teste.com']}
-        phones={['(51) 9-9999-8877']}
+      <Footer
+        FooterContactInfoProps={{
+          address: 'Avenida Praia de Belas, 1212, sala 424, RS - Brasil',
+          registration: 'CNPJ: 36.605.433/0001-18',
+          emails: ['atendimento@institutoeg.com'],
+          phones: ['(51) 9-9143-1009'],
+        }}
+        FooterIconsProps={{
+          facebook: 'https://www.facebook.com/InstitutoGnosisEducacional/',
+          instagram: 'https://www.instagram.com/gnosisinstitutoeducacional/',
+          twitter: 'https://twitter.com/GnosisInstituto',
+          youtube:
+            'https://www.youtube.com/channel/UC4qx3U-Hk8qlX46zTZqrL0w/videos?view_as=subscriber',
+          linkedin: 'https://institutoeg.com/',
+          whatsApp: 'https://wa.link/3in1jl',
+        }}
+        FooterLinksProps={{
+          label: 'Informações',
+          links: ['Sobre nós', 'Institucional', 'E-MEC'],
+        }}
       />
     </Box>
   );
